2f00ee24964015143297098313b6feedb395a744
Sriram Ravula <sriram.ravula@utexas.edu>
2017-03-19 20:55:22 -0500

    Updated to remove static OrderLog

--- hw4/src/Order.java
+++ hw4/src/Order.java
@@ -1,5 +1,4 @@
 //am73676_sr39533
-import java.util.ArrayList;
 
 public class Order {
 	
@@ -8,7 +7,7 @@ public class Order {
 	private int quantity;
 	private int id;
 	
-	private static OrderLog log = new OrderLog();
+	private static OrderLog log = new OrderLog(); //used to lock ID assignment
 	private static int currentId = 1;
 	
 	public Order(String user, String product, int quantity){
@@ -19,7 +18,6 @@ public class Order {
 		this.user = user;
 		this.product = product;
 		this.quantity = quantity;
-		log.add(this); //automatically add this new order to the log of orders
 	}
 	
 	public int getId(){
@@ -36,24 +34,5 @@ public class Order {
 	
 	public int getQuantity(){
 		return quantity;
-	}
-	
-	//cancels an order by removing it from the log
-	//returns whether or not the cancel was successful
-	public static boolean cancel(int id){
-		Order o = log.cancel(id);
-		
-		return o != null;
-	}
-	
-	//Given a user, returns a formatted String of all his orders
-	//If the user has no orders, returns a failure message
-	public static String search(String user){
-		ArrayList<Order> o = log.search(user);
-		
-		if(o.size() > 0)
-			return log.ordersToString(o);
-		
-		return "No order found for " + user;
 	}
 }
