dc0a1770874b2c24db9f5387d26a3e186ce5c31d
Sriram Ravula <sriram.ravula@utexas.edu>
2017-03-20 13:35:17 -0500

    Added comparisons for queueing

--- hw4/src/LogicalClock.java
+++ hw4/src/LogicalClock.java
@@ -1,7 +1,8 @@
 
-public class LogicalClock {
+public class LogicalClock implements Comparable<LogicalClock>{
 	
-	static int clock;
+	int clock;
+	int serverID;
 	
 	public LogicalClock (){
 		clock = 1;
@@ -15,8 +16,12 @@ public class LogicalClock {
 		clock++;
 	}
 	
-	public synchronized static int getClock(){
+	public synchronized int getClock(){
 		return clock;
+	}
+	
+	public synchronized int getID(){
+		return serverID;
 	}
 	
 	public synchronized void recieveAction(int other){
@@ -24,5 +29,28 @@ public class LogicalClock {
 			clock++;
 		else
 			clock = other + 1;
+	}
+	
+	public synchronized void recieveAction(LogicalClock other){
+		if(clock > other.clock)
+			clock++;
+		else
+			clock = other.clock + 1;
+	}
+
+	@Override
+	public int compareTo(LogicalClock arg0) {
+		if(clock < arg0.clock)
+			return -1;
+		else if (clock > arg0.clock)
+			return 1;
+		else{
+			if(serverID < arg0.serverID)
+				return -1;
+			else if (serverID == arg0.serverID)
+				return 0;
+			else
+				return 1;
+		}
 	}
 }
